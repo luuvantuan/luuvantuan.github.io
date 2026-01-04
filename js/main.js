b93edb87379e176b0a9e8f6357e678eed97469dc
Lưu Văn Tuấn <143016663+luuvantuan@users.noreply.github.com>
2026-01-04 07:36:36 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+const postFiles = [
+  "posts/bai-1.html"
+];
+
+const list = document.getElementById("post-list");
+
+postFiles.forEach(file => {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      const temp = document.createElement("div");
+      temp.innerHTML = html;
+
+      const article = temp.querySelector("article");
+
+      const title = article.dataset.title;
+      const date = article.dataset.date;
+      const desc = article.dataset.desc;
+
+      const item = document.createElement("div");
+      item.className = "post";
+
+      item.innerHTML = `
+        <h2>
+          <a href="post.html?file=${file}">
+            ${title}
+          </a>
+        </h2>
+        <small>${date}</small>
+        <p>${desc}</p>
+      `;
+
+      list.appendChild(item);
+    });
+});
 
